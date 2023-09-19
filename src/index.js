89b5410fc09b5caad5ce7f99b6fc6abd3c14af13
BHObserver <BHObserver0@gmail.com>
2023-09-19 17:04:27 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import App from './components/app';
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-
-const App = () => React.createElement('h1');
-
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
