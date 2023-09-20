561ad7b13599f78ce33efa6637253050a7f112cc
BHObserver <BHObserver0@gmail.com>
2023-09-20 11:00:16 +0600

    Update calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,33 +7,33 @@ const Calculator = () => (
     <div className="cal-container">
       <Output />
       <span className="btn-container-one">
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="bg-color">÷</button>
+        <button type="button" name="AC">AC</button>
+        <button type="button" name="+/-">+/-</button>
+        <button type="button" name="%">%</button>
+        <button type="button" name="/" className="bg-color">÷</button>
       </span>
       <span className="btn-container-two">
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="bg-color">×</button>
+        <button type="button" name="7">7</button>
+        <button type="button" name="8">8</button>
+        <button type="button" name="9">9</button>
+        <button type="button" name="*" className="bg-color">×</button>
       </span>
       <span className="btn-container-three">
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="bg-color">-</button>
+        <button type="button" name="4">4</button>
+        <button type="button" name="5">5</button>
+        <button type="button" name="6">6</button>
+        <button type="button" name="-" className="bg-color">-</button>
       </span>
       <span className="btn-container-four">
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="bg-color">+</button>
+        <button type="button" name="1">1</button>
+        <button type="button" name="2">2</button>
+        <button type="button" name="3">3</button>
+        <button type="button" name="+" className="bg-color">+</button>
       </span>
       <span className="btn-container-five">
-        <button type="button" className="zero">0</button>
-        <button type="button">.</button>
-        <button type="button" className="bg-color">=</button>
+        <button type="button" name="0" className="zero">0</button>
+        <button type="button" name=".">.</button>
+        <button type="button" name="=" className="bg-color">=</button>
       </span>
     </div>
   </div>
