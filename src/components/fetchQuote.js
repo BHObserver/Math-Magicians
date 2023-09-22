a5953e3dcab5c7d4a1db4783dd74a094501b4d1f
BHObserver <BHObserver0@gmail.com>
2023-09-22 23:12:17 +0600

    Update fetchQuote.js

--- src/components/fetchQuote.js
+++ src/components/fetchQuote.js
@@ -1,11 +1,12 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 
-function QuoteDisplay({ category, apiKey }) {
+function QuoteDisplay() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const category = 'Life'; // Default category
+  const apiKey = 'T4jhMp6jkPwaYLrw2IN+dg==9djzc72tDHZm9ypC'; // Default API key
 
   useEffect(() => {
     if (!apiKey) {
@@ -35,7 +36,7 @@ function QuoteDisplay({ category, apiKey }) {
         setError(error);
         setLoading(false);
       });
-  }, [category, apiKey]);
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -56,23 +57,9 @@ function QuoteDisplay({ category, apiKey }) {
         Quotes on &nbsp;
         {category}
       </h1>
-      <ul>
-        {quotes.map((quote) => (
-          <li key={quote.id}>{quote.text}</li>
-        ))}
-      </ul>
+      <span>{quotes[0].quote}</span>
     </div>
   );
 }
-
-QuoteDisplay.propTypes = {
-  category: PropTypes.string,
-  apiKey: PropTypes.string,
-};
-
-QuoteDisplay.defaultProps = {
-  category: 'Life',
-  apiKey: 'T4jhMp6jkPwaYLrw2IN+dg==9djzc72tDHZm9ypC',
-};
 
 export default QuoteDisplay;
