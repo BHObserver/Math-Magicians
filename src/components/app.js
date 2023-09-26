8da57ef9e82d4b74f74c58548e859e37173ec4d4
BHObserver <BHObserver0@gmail.com>
2023-09-26 12:08:48 +0600

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Calculator from './calculator';
 
 function App() {
   return (
-    <div className="app">
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav className="navbar">
+          <div className="navbar-brand">Math Magicians</div>
+          <ul className="navbar-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/">Calculator</Link></li>
+            <li><Link to="/">Quotes</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <div className="app">
+        <Calculator />
+      </div>
+    </BrowserRouter>
   );
 }
 
