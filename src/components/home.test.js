bb284466649775474f70cab3b9d1259029231ca5
BHObserver <BHObserver0@gmail.com>
2023-09-27 04:59:30 +0600

    Create home.test.js

---
+++ src/components/home.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Home from './home';
+
+test('renders the Home component', () => {
+// Check if the Home component is rendered
+  render(<Home />);
+});
