becbeb8fcf9161265f147ea72c78333190a538fa
BHObserver <BHObserver0@gmail.com>
2023-09-26 14:44:35 +0600

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,31 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import {
+  Link,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Calculator from './calculator';
 import QuoteDisplay from './fetchQuote';
+import Home from './home';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <header>
         <nav className="navbar">
-          <div className="navbar-brand">Math Magicians</div>
+          <div className="navbar-heading">Math Magicians</div>
           <ul className="navbar-links">
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/">Calculator</Link></li>
-            <li><Link to="/">Quotes</Link></li>
+            <li><Link to="/calculator">Calculator</Link></li>
+            <li><Link to="/fetchQuote">Quotes</Link></li>
           </ul>
         </nav>
       </header>
-      <div className="app">
-        <Calculator />
-      </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/fetchQuote" element={<QuoteDisplay />} />
+      </Routes>
+    </>
   );
 }
 
