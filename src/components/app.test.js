d08afc8f6eebb9e669352e42886dcaabee34a99c
BHObserver <BHObserver0@gmail.com>
2023-09-27 05:18:30 +0600

    Update app.test.js

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router-dom';
-import App from './app';
+import App from './App';
 
-test('navigates to Home when Home link is clicked', () => {
-  render(
-    <MemoryRouter initialEntries={['/']}>
-      <App />
-    </MemoryRouter>,
-  );
+test('App component snapshot', () => {
+  const tree = renderer
+    .create(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
