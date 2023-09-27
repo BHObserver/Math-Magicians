d901ae1a9a4e088b4ddf3088b38020124aa3e0e0
Karan Joshi <Karan190213@gmail.com>
2023-09-27 14:44:03 +0530

    Update operate.test.js file with DRY rule

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,32 +1,16 @@
 import operate from './operate';
 
 describe('operate function', () => {
-  it('should add two numbers', () => {
-    expect(operate(2, 3, '+')).toBe('5');
-  });
-
-  it('should subtract two numbers', () => {
-    expect(operate(5, 3, '-')).toBe('2');
-  });
-
-  it('should multiply two numbers', () => {
-    expect(operate(2, 3, 'x')).toBe('6');
-  });
-
-  it('should divide two numbers', () => {
-    expect(operate(6, 2, '÷')).toBe('3');
-  });
-
-  it('should handle division by zero', () => {
-    expect(operate(5, 0, '÷')).toBe("Can't divide by 0.");
-  });
-
-  it('should find modulo', () => {
-    expect(operate(7, 3, '%')).toBe('1');
-  });
-
-  it('should handle modulo with division by zero', () => {
-    expect(operate(10, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+  test.each([
+    [2, 3, '+', '5'],
+    [5, 3, '-', '2'],
+    [2, 3, 'x', '6'],
+    [6, 2, '÷', '3'],
+    [5, 0, '÷', "Can't divide by 0."],
+    [7, 3, '%', '1'],
+    [10, 0, '%', "Can't find modulo as can't divide by 0."],
+  ])('should perform %s %s %s = %s', (num1, num2, operation, expected) => {
+    expect(operate(num1, num2, operation)).toBe(expected);
   });
 
   it('should throw an error for an unknown operation', () => {
